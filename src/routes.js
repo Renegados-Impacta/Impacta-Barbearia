9f010b3b191a9dd981f6a8836164553e44ca63b1
Davimrp <davipereirar39@gmail.com>
2025-05-26 09:45:47 -0300

    Atualização 'routes.js'

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
-import Login from "./pages/Login/login";
-import Register from "./pages/Register/register";
-import RedefinirSenha from "./pages/RedefinirSenha/redefinirsenha";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import RedefinirSenha from "./pages/RedefinirSenha/RedefinirSenha";
 
-import Home from "./pages/Home/home";
-import HomePage from "./pages/HomePage/homepage";
+import Home from "./pages/Home/Home";
+import HomePage from "./pages/HomePage/HomePage";
 import PageAdmin from "./pages/PageAdmin/pageadmin";
 import Produtos from "./pages/Produtos/produtos";
 
